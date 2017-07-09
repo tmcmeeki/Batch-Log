d258ae70524df58be38565b2d3b5084740ea3d97
Tom <tmcmeeki@cpan.org>
2017-07-09 10:52:23 +1000

    unit_`date`

--- lib/Batch/Log.pm
+++ lib/Batch/Log.pm
@@ -1,60 +1,352 @@
 package Batch::Log;
-
-use 5.010000;
-use strict;
-use warnings;
-
-our @ISA = qw();
-
-our $VERSION = '0.01';
-
-
-# Preloaded methods go here.
-
-1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
-
+#
+# Batch::Log.pm - simple wrapper for Log::Log4perl.
+# $Revision: 1.3 $, Copyright (C) 2011 Thomas McMeekin
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 2 of the License,
+# or any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
 =head1 NAME
 
-Batch::Log - Perl extension for blah blah blah
+Batch::Log - simple wrapper for Log::Log4perl.
 
 =head1 SYNOPSIS
 
-  use Batch::Log;
-  blah blah blah
+  use Batch::Log qw/ :all /;
+
+  my $log = get_logger("mylog");
+
+  $log->debug("got here");
+
+  set_level($log, 'OFF');
 
 =head1 DESCRIPTION
 
-Stub documentation for Batch::Log, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+Batch::Log.pm - simple wrapper for Log::Log4perl.
 
-Blah blah blah.
+This is a simple wrapper for B<Log4perl> which sets up logging to both
+file and terminal simultaneously.
+
+It performs automatic configuration of logging locations and formats, e.g.
+the log file's path is derived automatically.
+
+It bases the initial logging format on the value of the environment variable 
+B<DEBUG> which can be set to any of the following values:
+
+  OFF TRACE DEBUG INFO WARN ERROR FATAL ALL
+
+If the variable is not specified, logging will assume a level of INFO.
+
+The default directory for logs is the user's $HOME/log directory, and 
+the filename will  resemble the program name (but with a '.log' extension),
+
+The logging level can be modified at run-time using the B<set_level> function,
+per below.  Note that the level of logging will also determine the format of
+the log message, e.g. TRACE and DEBUG logging has more information 
+embedded in the message than INFO logging.
+
+=head2 PUBLIC ROUTINES (exported)
+
+=over 4
+
+=item 1a.  get_logger(EXPR)
+
+Retrieve the logger object for use in standard logging operations.  This
+is equivalent to the Log4perl routine of the name name.  You should be
+able to use this object just as you would a B<Log4perl> object.
+
+This routine will setup all appenders and formats based on the value
+of the DEBUG shell variable at the time of the call.
+
+=item 1b.  set_level([LEVEL])
+
+This routine allows you to modify the logging level/format at run-time.
+If the LEVEL parameter is unspecified it will take its value from the DEBUG
+shell variable.  
+
+Note that the LEVEL parameter is a string rather than an integer, and will
+be translated by this module into a value as recognised by Log4perl.
+
+=back
+
+=head2 OTHER ROUTINES (not exported)
+
+=over 4
+
+=item 2a.  default_level([LEVEL])
+
+Reports the default logging level.  Does not modify any internal variables.
+
+=item 2b.  get_level
+
+Reports the current logging level being used by all appenders.
+
+=item 3a.  get_path
+
+Reports the current pathname of the logging file.
+
+=item 3b.  set_path
+
+Establishes the default pathname for the logging file and reports it.
+Note that this is automatically run as soon as this module is loaded.
+
+=item 4a.  get_pattern
+
+Reports the current message pattern being used by all appenders.
+
+=item 4b.  set_pattern([LEVEL])
+
+Modifies the current message pattern to that of the LEVEL specified. 
+If LEVEL is not specified then a default will apply.  All appenders
+will then be updated to the new pattern.
+
+=back
+
+=cut
+
+use strict;
+
+use Data::Dumper;
+use Exporter;
+use File::Basename;
+use File::Spec;
+use Log::Log4perl qw/ :levels :no_extra_logdie_message /;
+#use Module::Loaded;
 
 
-=head1 SEE ALSO
+# ---- package constants ----
+use constant DN_LOG => File::Spec->catfile($ENV{'HOME'}, "log");
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+use constant EXT_LOG => "log";
 
-If you have a mailing list set up for your module, mention it here.
+use constant P_NORMAL => '%d %p %F{1} %m%n';
+use constant P_DEBUG => '%d %p %F{2}(%L) %M{2} %m%n';
+use constant P_TRACE => '%d %p pid=%P %T %m%n';
 
-If you have a web site set up for your module, mention it here.
+
+# ---- package globals ----
+our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+
+
+# ---- package locals ----
+my %_setting;
+
+my @_appender = (
+	{ 'name' => "TermBatch::Log", 'type' => "Screen" },
+	{ 'name' => "FileBatch::Log", 'type' => "File" },
+
+#	---> add more appenders here if need be
+);
+
+# ---- sub-routines ----
+BEGIN {
+        $VERSION = sprintf "%d.%03d", q$Revision: 1.3 $ =~ /(\d+)/g;
+
+        @ISA = qw(Exporter Log::Log4perl);
+
+        @EXPORT_OK = qw(get_logger set_level OFF TRACE DEBUG INFO WARN ERROR FATAL ALL);
+
+	%EXPORT_TAGS = (
+		levels => [qw/ OFF TRACE DEBUG INFO WARN ERROR FATAL ALL /],
+		all => [qw/ get_logger set_level OFF TRACE DEBUG INFO WARN ERROR FATAL ALL /],
+	);
+}
+
+
+#INIT { };
+
+
+sub _create_appenders {
+	my $log = shift;
+	my $c_appender = "Log::Log4perl::Appender";
+
+	for my $record (@_appender) {
+
+		my $type = $record->{'type'};
+		my $id = $record->{'name'};
+		my $class = $c_appender . "::" . $type;
+
+#		print "class [$class] type [$type]\n";
+#		printf "_setting [%s]\n", Dumper(\%_setting);
+#		printf "set_path [%s]\n", set_path();
+
+		my $oa = (); 	# reset
+
+		if ($type eq 'File') {
+
+			$oa = $c_appender->new($class, 'name' => $id,
+				'filename' => $_setting{'path'});
+
+		} elsif ($type eq 'Screen') {
+
+			$oa = $c_appender->new($class, 'name' => $id,
+				'stderr' => 0, 'utf8' => 0);
+
+		}
+#		print "ref(oa) " . ref($oa) . "\n";
+
+		$log->add_appender($oa);
+
+		$record->{'object'} = $oa;	# remember for later
+	}
+#	printf "_appender [%s]\n", Dumper(\@_appender);
+}
+
+
+sub _get_setting {
+	my $setting = shift;
+
+	unless (exists $_setting{$setting}) {
+		return undef;
+	}
+
+	return $_setting{$setting};
+}
+
+
+sub default_level {
+	my $level = shift;	# optional
+
+	$level = exists($ENV{'DEBUG'}) ? $ENV{'DEBUG'} : "INFO"
+		unless (defined $level);
+
+	return $level;
+}
+
+
+sub get_level {
+
+	return _get_setting('level');
+}
+
+
+sub set_level {
+	my $log = shift;
+	my $level = default_level(shift);
+
+	set_pattern($level);
+
+	# convert to a numeric value and thus validate!
+
+	my $level_num = Log::Log4perl::Level::to_priority($level);
+
+##	printf "\t\txxx level [$level] level_num [%ld]\n", $level_num;
+
+	$log->level($level_num);
+
+	$_setting{'level'} = $level;
+
+	return $level;
+}
+
+
+sub get_path {
+
+	return _get_setting('path');
+}
+
+
+sub set_path {
+	my $ext = EXT_LOG;
+	my $fn_log = basename($0);
+
+	$fn_log =~ s/(pl|t|pm)$/$ext/;
+
+	my $pn_log = (-d DN_LOG) ? File::Spec->catfile(DN_LOG, $fn_log) : $fn_log;
+
+	$_setting{'path'} = $pn_log;
+
+	return $pn_log;
+}
+
+
+sub get_logger {
+	my $log = Log::Log4perl::get_logger(shift);
+
+	set_path();
+
+	_create_appenders($log);
+
+	set_level($log);
+
+	return $log;
+}
+
+
+sub get_pattern {
+
+	return _get_setting('pattern');
+}
+
+
+sub set_pattern {
+	my $level = default_level(shift);
+
+	# establish a default pattern
+
+	# levels: TRACE DEBUG INFO WARN ERROR FATAL
+	my %pattern = ( 'INFO' => P_NORMAL, 'TRACE' => P_TRACE );
+
+	my $pattern = exists($pattern{$level}) ? $pattern{$level} : P_DEBUG;
+
+#	print "\t\txxx pattern [$pattern]\n";
+
+	# apply the pattern layout to my registered appenders
+
+	my $layout = Log::Log4perl::Layout::PatternLayout->new($pattern);
+
+	for my $record (@_appender) {
+
+		my $name = $record->{'name'};
+		my $oa = $record->{'object'};
+
+#		printf "name [$name] ref [%s] layout [%s]\n", ref($oa), ref($layout);
+
+		$oa->layout($layout);
+	}
+
+	$_setting{'pattern'} = $pattern;
+
+	return $pattern;
+}
+
+
+#END { }
+
+1;
+
+__END__
+
+=head1 TO-DO
+
+1. Test creation and disablement of appenders after initial creation, i.e.
+dynamic appender creation.
+
+2. Create a routine for a Tk-style logger, i.e. logging to a widget which
+should necessarily disable the STDOUT appender.
+
+=head1 VERSION
+
+$Revision: 1.3 $
 
 =head1 AUTHOR
 
-U-matisse\tomby, E<lt>tomby@nonetE<gt>
+Copyright (C) 2011  Tom McMeekin
 
-=head1 COPYRIGHT AND LICENSE
+=head1 SEE ALSO
 
-Copyright (C) 2017 by U-matisse\tomby
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.22.3 or,
-at your option, any later version of Perl 5 you may have available.
-
+L<perl>, L<Log::Log4perl>.
 
 =cut
+
--- t/00basic.t
+++ t/00basic.t
@@ -1,76 +1,169 @@
-# $Header$
-#
-# 00basic.t - test harness for Batch::Exec - basic tests
-#
-# History:
-# $Log$
-use Log::Log4perl qw/ :easy /; Log::Log4perl->easy_init($ERROR);
-
+use Test::More tests => 191;
+use Module::Loaded;
 use Data::Dumper;
-use File::Glob;
-
-use Test::More tests => 31;
-
-BEGIN { use_ok('Batch::Exec') };
 
 
-# -------- global variables --------
-my $log = get_logger(__FILE__);
-my $c_this = "Batch::Exec";
-my $cycle = 1;
-my @parms = qw/ echo fatal prefix retry /;
-my $dummy = "foobar";
+# -------- sub-routines --------
+sub linecount {	# do a line-count on the file passed
+	my $pn = shift;
+	my $ra = shift;
+	my $lc = 0;
 
+	@$ra = ()
+		if (defined $ra);
 
-# -------- new / initilaisation --------
-my $obj1 = Batch::Exec->new;
-my $obj2 = Batch::Exec->new( map{ $_ => $dummy } @parms);
+	open(FH, "<$pn") || die "open($pn) read failed";
 
-$log->info("creating new $c_this objects...");
+	while (<FH>) {
+		$lc++;
 
-isa_ok( $obj1, $c_this,	'new no args');
-isa_ok( $obj2, $c_this,	'new with args');
+		chomp;
 
+		push @$ra, $_
+			if (defined $ra);
+	}
 
-# -------- simple attributes: numbers --------
-for my $parm (@parms) {
+	close(FH) || die "close($pn) failed";
 
-	isnt($obj1->$parm, -1,			"default $parm cycle_$cycle");
-
-	my $default = $obj1->$parm;
-	my $assign = !$default;
-
-	is($obj1->$parm($assign), $assign,	"assign $parm cycle_$cycle");
-	isnt($obj1->$parm, $default,		"check $parm cycle_$cycle");
-
-	$log->debug(sprintf "parm [$parm]=%d", $obj1->$parm);
-
-	ok($obj1->$parm >= 0,			"integer $parm cycle_$cycle");
-	is($obj1->$parm($default), $default,	"reset $parm cycle_$cycle");
-
-	ok($obj2->$parm eq $dummy,		"dummy $parm cycle_$cycle");
-	ok($obj1->$parm ne $obj2->$parm,	"override $parm cycle_$cycle");
-
-        $cycle++;
+	return $lc;
 }
 
 
+sub truncate_file {	# truncate the file passed
+	my $pn = shift;
+
+	open(FH, ">$pn") || die "open($pn) write failed";
+	close(FH) || die "close($pn) failed";
+
+	return linecount($pn);
+}
+
+
+sub is_loggable {
+	my ($lvl1, $func)=@_;
+	my $lvl2 = uc $func;
+
+	my $nl1 = Log::Log4perl::Level::to_priority($lvl1);
+	my $nl2 = Log::Log4perl::Level::to_priority($lvl2);
+
+	return 1 if (Log::Log4perl::Level::isGreaterOrEqual($nl1, $nl2));
+
+	return 0;
+}
+
+
+# -------- global --------
+BEGIN { use_ok('Batch::Log', qw/ :all /) };
+my $log = get_logger(__FILE__);
+isa_ok($log, "Log::Log4perl::Logger");
+
+
+# -------- path --------
+isnt(Batch::Log::set_path, "",		"set_path non-null");
+my @tokens = split(/\./, __FILE__);
+my $re = $tokens[0];
+like(Batch::Log::set_path, qr/$re/,	"set_path prefix");
+like(Batch::Log::set_path, qr/log/,	"set_path suffix");
+
+my $pn = Batch::Log::get_path;
+isnt($pn, "",				"get_path non-null");
+ok(-f $pn == 1,				"get_path exists");
+ok(-w $pn == 1,				"get_path writable");
+
+is(truncate_file($pn), 0,		"truncate initial");
+
+
+# -------- miscellaneous --------
+isnt( is_loaded('Log::Log4perl'), "",		"loaded 0");
+isnt( is_loaded('Batch::Log'), "",		"loaded 1");
+
+isnt( is_loaded('Tk'), 0,			"not_loaded 0");
+isnt( is_loaded('Log::Dispatch::TkText'), 0,	"not_loaded 1");
+
+is(Batch::Log::default_level, "INFO",		"default_level default");
+is(Batch::Log::default_level("XXX"), "XXX",	"default_level override");
+is(Batch::Log::default_level, "INFO",		"default_level reset");
+
+is(Batch::Log::get_level, "INFO",		"get_level default");
+
+delete $ENV{'DEBUG'};
+isnt(exists $ENV{'DEBUG'}, 1,			"clear environment");
+is(set_level($log), "INFO",			"set_level default");
+my $rep = '\%.+n$';
+like(Batch::Log::get_pattern, qr/$rep/,			"get_pattern default");
+
+like(Batch::Log::set_pattern, qr/$rep/,			"set_pattern default");
+like(Batch::Log::set_pattern("INFO"), qr/$rep/,		"set_pattern override");
+like(Batch::Log::set_pattern("INVALID"), qr/$rep/,	"set_pattern invalid");
+
+
+# -------- setup functions and levels --------
+my @levels = qw/ OFF TRACE DEBUG INFO WARN ERROR FATAL ALL /;
+my @functions = map { lc $_ } @levels;
+pop @functions;
+shift @functions;
+
+#printf "functions [%s] levels [%s]\n", Dumper(\@functions), Dumper(\@levels);
+
+# -------- test log functions at all levels --------
+my $cycle = 0;
+for my $level (@levels) {
+
+	$ENV{'DEBUG'} = $level;
+
+	is(set_level($log), $level,		"set_level   cycle $cycle shell=$level");
+	is(set_level($log, $level), $level,	"set_level   cycle $cycle set=$level");
+
+	is(Batch::Log::get_level, $level,	"get_level   cycle $cycle $level");
+	for my $func (@functions) {
+
+		my $what = "cycle=$cycle level=$level func=$func";
+
+		$log->$func("'message $what'");
+
+		my $lc = undef;
+		if ($level eq 'ALL') {
+			$lc = 1;
+		} elsif ($level eq 'OFF') {
+			$lc = 0;
+		} else {
+			$lc =is_loggable($level, $func); 
+		}
+
+		my @grep;
+
+		is(linecount($pn, \@grep), $lc,	"linecount=$lc $what");
+
+		my $re=uc " $func ";
+		my @match = grep /$re/, @grep;
+		is(@match, $lc,			"grep $what");
+
+
+		is(truncate_file($pn), 0,	"truncate    $what");
+		$cycle++;
+	}
+}
+
+#$log->info(sprintf "log [%s]", Dumper($log));
+#$log->info(sprintf "appenders [%s]", Dumper(Log::Log4perl::appenders));
+
+
 # -------- finalise --------
-$log->info("processing complete.");
+#$log->info("processing complete.");
 
 __END__
 
 =head1 DESCRIPTION
 
-00basic.t - test harness for Batch::Exec - basic tests
+00basic.t - test harness for Batch::Log - basic tests
 
 =head1 VERSION
 
-$Revision: 1.7 $
+SEE DISTRIBUTION NOTES
 
 =head1 AUTHOR
 
-Copyright (C) 2017  B<Tom McMeekin> tmcmeeki@cpan.org
+Copyright (C) 2011  B<Tom McMeekin> tmcmeeki@cpan.org
 
 =head1 LICENSE
 
